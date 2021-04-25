0f92844088a7473d3fccf91ac376e9987199642a
Arun Yadav <7602213-arunyadav_vnnovate@users.noreply.gitlab.com>
2021-04-26 00:19:11 +0530

    fix service issue

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Interfaces\LedgerInterface;
 use App\Services\Services;
 use App\Traits\LedgerTraits;
 use Illuminate\Http\Request;
@@ -11,7 +9,7 @@ use Illuminate\Http\Request;
 class ServiceController extends Controller
 {
     use LedgerTraits;
-    public function index(LedgerInterface $ledger){
+    public function index(Services $ledger){
         logger($this->showBalance());
         $ledger
         ->setYear(2021)
--- app/Services/Services.php
+++ app/Services/Services.php
@@ -3,7 +3,7 @@ namespace App\Services;
 
 use App\Interfaces\LedgerInterface;
 
-class Services implements LedgerInterface
+class Services
 {
     private $year;
 
